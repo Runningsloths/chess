6a768206cb6e7e3ed46827a18fd1800f11be3365
Runningsloths <32084733+Runningsloths@users.noreply.github.com>
2018-05-10 09:38:28 -0700

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,11 +1,12 @@
 import java.util.ArrayList;
 public class Game
 {
+	public static final BOARD_LENGTH = 8;
 	public piece[][] board;
 	public Game(){
-		board = new Piece[8][8];
-		for(int a = 0; a < 8; a++){
-			for(int b = 0; b < 8; b++){
+		board = new Piece[BOARD_LENGTH][BOARD_LENGTH];
+		for(int a = 0; a < BOARD_LENGTH; a++){
+			for(int b = 0; b < BOARD_LENGTH; b++){
 				board[a][b] = null;
 			}
 		}
