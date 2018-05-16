2defc696153aa2ec6e1dde049ab373bdd66a4b55
Falcons-Royale <36275109+Falcons-Royale@users.noreply.github.com>
2018-05-16 09:16:14 -0700

    Create Rook.java

---
+++ Rook.java
@@ -0,0 +1,62 @@
+public class Rook implements Piece{
+	int Rows;
+	int Cols;
+	Position pos;
+	boolean alive;
+	boolean c;
+	int move;
+	public Rook(int x, int y, boolean color){
+	  Rows = x;
+	  Cols = y;
+	  pos = new Position(Rows, Cols);
+	  alive = false;
+	  c = color;
+	  move = 0;
+	}
+	public void implementMove(){
+		move++;
+	}
+	public ArrayList<Position> getMoves(Piece[][] board){
+		ArrayList<Position> m = new ArrayList<Position>();
+		if(move == 0){
+			if(board[pos.getX()-2][pos.getY()] == null){
+				m.add(new Position(pos.getX()-2, pos.getY()));
+			}
+		}
+		if(pos.getX()-1<=0){
+			if(board[pos.getX()-1][pos.getY()] == null){
+				m.add(new Position(pos.getX() - 1, pos.getY()));
+			}
+			if(pos.getY()-1<=0||pos.getY()+1<Cols){
+				if(board[pos.getX()-1][pos.getY()+1] != null&&board[pos.getX()-1][pos.getY()+1].bOrW() != c){
+					m.add(new Position(pos.getX()-1, pos.getY()+1));
+				}
+				if(board[pos.getX()-1][pos.getY()-1] != null&&board[pos.getX()-1][pos.getY()-1].bOrW() != c){
+					m.add(new Position(pos.getX()-1, pos.getY()-1));
+				}
+			}
+		}
+	}
+	public int getID(){
+		return 1;
+	}
+	public Position getPos(){
+		return pos;
+	}
+	public void move(Position p){
+		pos = p;
+	}
+	public boolean isAlive(){
+		return alive;
+	}
+	public void setStatus(boolean s){
+		alive = s;
+	}
+	public boolean bOrW(){
+		return c;
+	}
+	public String toString(){
+		return "Rook";
+	}
+}
+
